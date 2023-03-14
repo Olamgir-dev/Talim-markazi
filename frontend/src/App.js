042693669b9079f8791b50ee34f7cf6a52350756
Ulug'bek <rashidov.ulugbek@internet.ru>
2023-03-14 13:40:42 +0500

    Frontend qilindi
    O'qituvchi qoshish , O'quvchi qoshish

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import TeacherAdd from './Routers/TeacherAdd';
+import StudentAdd from './Routers/StudentAdd';
+import "./sass/style.scss"
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path='/teacher-add' element={<TeacherAdd />}/>
+          <Route path='/student-add' element={<StudentAdd />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
