ca7da34de0971df68c94f0abffecc2e8241446f4
bo1jan <109594231+bo1jan@users.noreply.github.com>
2023-03-16 14:38:08 +0500

    add group-show

--- frontend/src/Routers/ShowGroups.js
+++ frontend/src/Routers/ShowGroups.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import GroupCard from '../components/GroupCard'
+import axios from 'axios'
+import Card from 'react-bootstrap/Card';
 
 function ShowGroups() {
+  const [examArray, setExamArray] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/group')
+      .then((res) => {
+        setExamArray(res.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [])
   return (
-    <div>ShowGroups</div>
+    <div>
+      {
+        examArray.length > 0 ?
+          <div className='flex-div'>
+            {examArray.map((group) => {
+              return <GroupCard key={group._id} group={group} />
+            })}
+          </div>
+          : <>
+            <Card.Text>
+              Loading...
+            </Card.Text>
+          </>
+      }
+    </div>
   )
 }
 
