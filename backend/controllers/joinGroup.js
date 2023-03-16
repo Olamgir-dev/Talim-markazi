c0f2cd58dbea7bc0e01aa5f66e1f5673357851b3
BLEF <121230919+BLEF72@users.noreply.github.com>
2023-03-16 15:38:10 +0500

    Update joinGroup.js

--- backend/controllers/joinGroup.js
+++ backend/controllers/joinGroup.js
@@ -4,7 +4,7 @@ const Student = require('../models/studentModel');
 const joinGroup = async (req, res) => {
     const {groupId, studentId} = req.body;
     try {
-        const student = await Student.findOneAndUpdate(groupId);
+        const student = await Student.findOneAndUpdate(groupId, studentId);
         res.status(200).json(student);
     }
     catch (error) {
