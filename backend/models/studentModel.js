1a4dbe121dabd11b2e4c0051c411fc8ff93e971c
Sayitqulov Suxrob <121230995+Suxrobsayitqulov@users.noreply.github.com>
2023-03-24 16:06:08 +0500

    Ozgartdim

--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -1,7 +1,6 @@
 const validator = require('validator');
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const validator = require("validator");
 const studentSchema = mongoose.Schema({
   firstname: {
     type: String,
